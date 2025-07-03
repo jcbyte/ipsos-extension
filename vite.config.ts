9bbbe9590439b7ad622e2111ff1f28f6215cd0ca
Joel Cutler <joelcutler108@gmail.com>
2025-07-03 23:05:21 +0100

    add boilerplate extension

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			input: {
+				background: "src/background.ts",
+			},
+			output: {
+				entryFileNames: "[name].js",
+			},
+		},
+	},
+});
