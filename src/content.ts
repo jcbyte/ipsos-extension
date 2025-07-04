3fa930ba288024151b5372000fe118509212c4ff
Joel Cutler <joelcutler108@gmail.com>
2025-07-04 17:43:42 +0100

    auto fill top date and time box with current time

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,21 @@
 import { waitForElement } from "./util";
 
 async function autofill() {
-	// Select yes to confirm information entered is accurate
+	// Auto-fill top date and time box with the current time
+	const topYearSelect = await waitForElement<HTMLSelectElement>("#dsYear");
+	const topMonthSelect = await waitForElement<HTMLSelectElement>("#dsMonth");
+	const topDaySelect = await waitForElement<HTMLSelectElement>("#dsDay");
+	const topHourSelect = await waitForElement<HTMLSelectElement>("#tsHoursHEAD_TIME");
+	const topMinuteSelect = await waitForElement<HTMLSelectElement>("#tsMinutesHEAD_TIME");
+
+	const now = new Date();
+	topYearSelect.value = now.getFullYear().toString();
+	topMonthSelect.value = now.getMonth().toString();
+	topDaySelect.value = (now.getDate() - 1).toString();
+	topHourSelect.value = now.getHours().toString();
+	topMinuteSelect.value = now.getMinutes().toString();
+
+	// Auto-select yes to confirm information entered is accurate
 	const confirmInformationInput = await waitForElement(() => {
 		const labels = document.body.querySelectorAll("span.surveyansweroption");
 
@@ -10,17 +24,19 @@ async function autofill() {
 		);
 
 		const targetInput = targetLabel?.parentElement?.querySelector("input");
-
 		return targetInput ?? null;
 	});
 	confirmInformationInput.click();
+
+	// Auto-fill main date and time box with current time
+	// todo
 }
 
 autofill();
 
 // todo
-// 0.1 fill date with current
-// 0.2 fill time with current
+// // 0.1 fill date with current
+// //; 0.2 fill time with current
 // // 1.1 select yes
 // 1.2 fill date with current (copy this to 0.1 or vice versa)
 // 1.3 fill time with current (copy this to 0.2 or vice versa)
@@ -29,5 +45,5 @@ autofill();
 // 4.1a fill with stored
 // 2.6.1 fill with calculated age from stored dob
 // 2.6.2 fill with calculated age from stored dob
-
+// todo make this better so it doesn't run sequently and only once
 // todo create popup to store data
--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,25 @@
 /**
- * Returns the first element that matches selectors once it appears in the DOM.
+ * Waits for a DOM element to appear in the document, either by selector string or selector function.
+ *
+ * @template T - The type of the element we are wait for.
+ * @param {string | (() => T | null)} selector - A CSS selector string or a function returning the desired element.
+ *   - If a string is provided, it uses `document.querySelector` on document changes to search for the element.
+ *   - If a function is provided, it will call the function on document changes until it returns a non-null element.
+ * @returns {Promise<T>} A promise that resolves with the element once it is found in the DOM.
  */
-export function waitForElement<T extends Element>(selectorFunc: () => T | null): Promise<T> {
+export function waitForElement<T extends Element>(selector: string): Promise<T>;
+export function waitForElement<T extends Element>(selectorFunc: () => T | null): Promise<T>;
+export function waitForElement<T extends Element>(selector: string | (() => T | null)): Promise<T> {
+	const getElement = typeof selector === "string" ? () => document.querySelector<T>(selector) : selector;
+
 	return new Promise((resolve) => {
 		// Immediate check
-		const el = selectorFunc();
+		const el = getElement();
 		if (el) return resolve(el);
 
 		// When the DOM is changed check for the element
 		const observer = new MutationObserver(() => {
-			const el = selectorFunc();
+			const el = getElement();
 			// If it now exists then resolve
 			if (el) {
 				observer.disconnect();
