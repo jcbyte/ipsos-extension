6f4ac27497816b50d504df5a06ef9db97e01903b
Joel Cutler <joelcutler108@gmail.com>
2025-07-04 21:34:01 +0100

    sync changes to date and time across parts

--- src/content.ts
+++ src/content.ts
@@ -68,33 +68,81 @@ function autofill() {
 		const formattedDate = `${day}/${month}/${year}`;
 
 		dateInput.value = formattedDate;
+
+		// Sync changes with top date
+		if (topYearSelect && topMonthSelect && topDaySelect) {
+			// We have to use a polling method here, as the calender selector updates `dateInput.value` silently
+			let lastDateInputValue = dateInput.value;
+			setInterval(() => {
+				if (dateInput.value !== lastDateInputValue) {
+					lastDateInputValue = dateInput.value;
+					const [day, month, year] = dateInput.value.split("/");
+
+					topYearSelect.value = year;
+					topMonthSelect.value = String(Number(month) - 1);
+					topDaySelect.value = String(Number(day) - 1);
+				}
+			}, 100);
+
+			const updateDateInput = () => {
+				const day = String(Number(topDaySelect.value) + 1).padStart(2, "0");
+				const month = String(Number(topMonthSelect.value) + 1).padStart(2, "0");
+				const year = topYearSelect.value;
+				const formattedDate = `${day}/${month}/${year}`;
+
+				dateInput.value = formattedDate;
+			};
+
+			topYearSelect.addEventListener("change", updateDateInput);
+			topMonthSelect.addEventListener("change", updateDateInput);
+			topDaySelect.addEventListener("change", updateDateInput);
+		}
 	} else {
 		console.warn("Ipsos Extension: Could not find date input.");
 	}
 
 	// Auto-fill main time with current time
 	const timeQuestion = questions.find((question) => question.textContent?.trim().startsWith("1.3"));
-	const timeInputs = timeQuestion?.parentElement?.querySelector("tbody")?.querySelectorAll<HTMLSelectElement>("select");
+	const timeSelects = timeQuestion?.parentElement
+		?.querySelector("tbody")
+		?.querySelectorAll<HTMLSelectElement>("select");
 
-	if (timeInputs && timeInputs.length === 2) {
-		const hourInput = timeInputs[0];
-		const minuteInput = timeInputs[1];
+	if (timeSelects && timeSelects.length === 2) {
+		const hourSelect = timeSelects[0];
+		const minuteSelect = timeSelects[1];
 
-		hourInput.value = now.getHours().toString().padStart(2, "0");
-		minuteInput.value = now.getMinutes().toString().padStart(2, "0");
+		hourSelect.value = now.getHours().toString().padStart(2, "0");
+		minuteSelect.value = now.getMinutes().toString().padStart(2, "0");
+
+		// Sync changes with top time
+		if (topHourSelect && topMinuteSelect) {
+			hourSelect.addEventListener("change", (e) => {
+				const newValue = (e.target as HTMLSelectElement).value;
+				const formattedValue = newValue.replace(/^0+/, "") || "0";
+				topHourSelect.value = formattedValue;
+			});
+
+			topHourSelect.addEventListener("change", (e) => {
+				const newValue = (e.target as HTMLSelectElement).value;
+				const formattedValue = newValue.padStart(2, "0");
+				hourSelect.value = formattedValue;
+			});
+		}
 	} else {
 		console.warn("Ipsos Extension: Could not find time inputs.");
+	}
+
+	// Sync changes to top date and time with main date and time
+	if (topYearSelect && topMonthSelect && topDaySelect && dateInput) {
 	}
 }
 
 awaitForm();
 
 // todo
-// copy date and times across
 // 1.5 automatically upload id, or link to location may be easier
 // 4.1 fill with stored
 // 4.1a fill with stored
 // 2.6.1 fill with calculated age from stored dob
 // 2.6.2 fill with calculated age from stored dob
-// todo make this better so it doesn't run sequently and only once
 // todo create popup to store data
