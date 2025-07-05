c67c68e123f22bda093d1d9c38eb6dade6dffa64
Joel Cutler <joelcutler108@gmail.com>
2025-07-05 19:44:55 +0100

    compile injected content script as iife and popup as es

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,47 @@
 import preact from "@preact/preset-vite";
-import { resolve } from "path";
-
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { resolve } from "path";
+import { defineConfig, UserConfig } from "vite";
 
-export default defineConfig({
-	build: {
-		outDir: "dist",
-		rollupOptions: {
-			input: {
-				content: "src/content.ts",
-				popup: "popup.html",
-			},
-			output: {
-				entryFileNames: "[name].js",
-			},
-		},
-	},
-	plugins: [preact(), tailwindcss()],
+const baseConfig: UserConfig = {
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, "src"),
 		},
 	},
-});
+};
+
+export default defineConfig(({ mode }) => ({
+	...baseConfig,
+	...(mode === "content"
+		? {
+				// Content script build - IIFE format
+				build: {
+					outDir: "dist/content",
+					copyPublicDir: false, // As this would place it within dist/content
+					rollupOptions: {
+						input: "src/content.ts",
+						output: {
+							entryFileNames: "[name].js",
+							format: "iife", // For injected content script
+						},
+					},
+					emptyOutDir: false,
+				},
+		  }
+		: {
+				// Popup build - ES module format
+				build: {
+					outDir: "dist",
+					rollupOptions: {
+						input: "popup.html",
+						output: {
+							entryFileNames: "[name].js",
+							format: "es",
+						},
+					},
+					emptyOutDir: false,
+				},
+				plugins: [preact(), tailwindcss()],
+		  }),
+}));
