52069d6c611ec12de35ccb47f0048acf0dee4d7a
Joel Cutler <joelcutler108@gmail.com>
2025-07-05 16:26:27 +0100

    added toggles to input boxes

--- src/popup/PopupPanel.tsx
+++ src/popup/PopupPanel.tsx
@@ -6,44 +6,45 @@ import { Switch } from "@/components/ui/switch";
 export default function PopupPanel() {
 	return (
 		<div class="min-w-80 p-6 flex flex-col gap-4">
-			<div class="flex flex-col gap-1">
-				<span class="text-xl font-bold">Ipsos Extension</span>
-				<span class="text-zinc-300 text-sm">
+			<div class="flex flex-col gap-1 items-center">
+				<span class="text-xl font-bold text-center">Ipsos Extension</span>
+				<span class="text-zinc-300 text-sm text-center">
 					Ipsos extension is for automating form filling and streamlining survey processes
 				</span>
 			</div>
 
 			<Separator />
 
-			<div class="flex flex-col gap-1">
+			<div class="flex flex-col gap-4">
 				{/* Auto-fill date and time toggle */}
-				<div className="flex items-center gap-2">
+				<div className="flex items-center justify-between gap-2">
 					<Label htmlFor="auto-fill-date-toggle">Auto-fill Date & Time</Label>
 					<Switch id="auto-fill-date-toggle" />
 				</div>
 
 				{/* Auto-confirm accuracy statement toggle */}
-				<div className="flex items-center gap-2">
+				<div className="flex items-center justify-between gap-2">
 					<Label htmlFor="auto-agree-accuracy-toggle">Auto-agree Accuracy Statement</Label>
 					<Switch id="auto-agree-accuracy-toggle" />
 				</div>
 
 				{/* ID file location input */}
-				<div className="grid w-full max-w-sm items-center gap-3">
-					<Label htmlFor="id-location-input">ID Location</Label>
-					<Input type="text" id="id-location-input" placeholder="C:\Docs\Id\" />
-				</div>
-
-				{/* Postcode input */}
-				<div className="grid w-full max-w-sm items-center gap-3">
-					<Label htmlFor="postcode-input">Email</Label>
-					<Input type="text" id="postcode-input" placeholder="SW1A 1AA" />
+				<div className="flex flex-col gap-2">
+					<div className="flex items-center justify-between gap-2">
+						<Label htmlFor="id-location-toggle">Copy ID path on upload</Label>
+						<Switch id="id-location-toggle" />
+					</div>
+					<Input type="text" id="id-location-input" placeholder="Enter ID file path" />
 				</div>
 
 				{/* Address input */}
-				<div className="grid w-full max-w-sm items-center gap-3">
-					<Label htmlFor="address-input">Email</Label>
-					<Input type="text" id="address-input" placeholder="Buckingham Palace" />
+				<div className="flex flex-col gap-2">
+					<div className="flex items-center justify-between gap-2">
+						<Label htmlFor="address-toggle">Auto-fill address</Label>
+						<Switch id="address-toggle" />
+					</div>
+					<Input type="text" id="postcode-input" placeholder="Enter postcode" />
+					<Input type="text" id="address-input" placeholder="Enter address line" />
 				</div>
 
 				{/* // todo add dob selector */}
@@ -51,9 +52,9 @@ export default function PopupPanel() {
 
 			<Separator />
 
-			<div class="flex flex-col gap-1">
-				<span>Version X.X.X</span>
-				<span>Made by Joel</span>
+			<div class="flex flex-col gap-0 items-center">
+				<span class="text-zinc-300 text-sm text-center">Version X.X.X</span>
+				<span class="text-zinc-300 text-sm text-center">Made by Joel</span>
 			</div>
 		</div>
 	);
