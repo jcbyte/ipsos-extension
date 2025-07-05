1e214e17c395695c657bfc63492efd21d0f75d96
Joel Cutler <joelcutler108@gmail.com>
2025-07-05 17:39:15 +0100

    update storage api to get settings with type definitions

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,9 +1,29 @@
-type StorageKey = "idLocation" | "postcode" | "address" | "dob";
+type SettingKey = "date" | "agreeAccuracy" | "idLocation" | "address" | "dob";
 
-export async function get<T>(key: StorageKey): Promise<T | undefined> {
-	return (await chrome.storage.local.get(key))[key] as T;
+type BaseSetting = { enabled: boolean };
+type ValueSetting<T> = BaseSetting & { value: T };
+
+type ExactSettingMap<T extends Record<SettingKey, BaseSetting>> = T;
+
+type SettingValueMap = ExactSettingMap<{
+	date: BaseSetting;
+	agreeAccuracy: BaseSetting;
+	idLocation: ValueSetting<string>;
+	address: ValueSetting<{ postcode: string; address: string }>;
+	dob: ValueSetting<string>;
+}>;
+
+export async function getSetting<T extends SettingKey>(key: T): Promise<SettingValueMap[T] | undefined> {
+	const setting = (await chrome.storage.local.get(key))[key];
+	if (!setting) return undefined;
+	return setting as SettingValueMap[T];
 }
 
-export async function set<T>(key: StorageKey, value: T): Promise<void> {
+export async function getSettings<T extends SettingKey>(keys: T[]): Promise<Partial<Pick<SettingValueMap, T>>> {
+	const result = await chrome.storage.local.get(keys);
+	return result as Partial<Pick<SettingValueMap, T>>;
+}
+
+export async function setSetting<T extends SettingKey>(key: T, value: SettingValueMap[T]): Promise<void> {
 	await chrome.storage.local.set({ [key]: value });
 }
